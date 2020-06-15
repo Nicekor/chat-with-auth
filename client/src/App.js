ae5a4975ff2f66d2e76fac71e4a2dfcbb86747ef
Nicekor <zemorais16@hotmail.com>
2020-06-15 18:27:54 +0100

    Added new routes and the AuthProvider to the App

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ import { getThemes } from './utils/colorThemes';
 
 import Header from './components/Header/Header';
 import LoginForm from './components/LogInForm/LogInForm';
+import Chat from './components/Chat/Chat';
 import NotFound from './components/NotFound/NotFound';
+import PrivateRoute from './hoc/PrivateRoute/PrivateRoute';
+import AuthProvider from './context/Auth';
+import EnterNickname from './components/EnterNickname/EnterNickname';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -13,16 +17,24 @@ function App() {
   const [darkTheme, lightTheme] = getThemes();
 
   return (
-    <MuiThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-        <Switch>
-          <Route path="/" exact component={LoginForm} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </MuiThemeProvider>
+    <AuthProvider>
+      <MuiThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Switch>
+            <Route path="/" exact component={LoginForm} />
+            <PrivateRoute path="/nickname">
+              <EnterNickname />
+            </PrivateRoute>
+            <PrivateRoute path="/chat">
+              <Chat />
+            </PrivateRoute>
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </MuiThemeProvider>
+    </AuthProvider>
   );
 }
 
