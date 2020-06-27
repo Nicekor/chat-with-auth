cc93dca39d9ac7c4488b76485ee44bc110a85645
Nicekor <zemorais16@hotmail.com>
2020-06-27 15:56:40 +0100

    Addressees and messages dummy data whilst I server side is not done

---
+++ client/src/utils/dummyData.js
@@ -0,0 +1,225 @@
+export const addressees = [
+  {
+    id: 1,
+    firstName: 'Nadia',
+    lastName: 'Lantaff',
+    lastMessage: 'test⁠test',
+  },
+  {
+    id: 2,
+    firstName: 'Willey',
+    lastName: 'Darke',
+    lastMessage: '1E02',
+  },
+  {
+    id: 3,
+    firstName: 'Valene',
+    lastName: 'Brindle',
+    lastMessage: "'\"'",
+  },
+  {
+    id: 4,
+    firstName: 'Tilly',
+    lastName: 'Wapplington',
+    lastMessage: '"',
+  },
+  {
+    id: 5,
+    firstName: 'Fabio',
+    lastName: 'Hollidge',
+    lastMessage: '١٢٣',
+  },
+  {
+    id: 6,
+    firstName: 'Brion',
+    lastName: 'Pavy',
+    lastMessage: "'\"'",
+  },
+  {
+    id: 7,
+    firstName: 'Reggie',
+    lastName: 'Honywill',
+    lastMessage: '1/0',
+  },
+  {
+    id: 8,
+    firstName: 'Bridget',
+    lastName: 'Bernardot',
+    lastMessage: "'\"'",
+  },
+  {
+    id: 9,
+    firstName: 'Cecil',
+    lastName: 'Duiguid',
+    lastMessage: '1/0',
+  },
+  {
+    id: 10,
+    firstName: 'Gar',
+    lastName: 'Flanagan',
+    lastMessage: 'null',
+  },
+  {
+    id: 11,
+    firstName: 'Jada',
+    lastName: 'Philippe',
+    lastMessage: '0/0',
+  },
+  {
+    id: 12,
+    firstName: 'Dalt',
+    lastName: 'Ealles',
+    lastMessage: 'Z̮̞̠͙͔ͅḀ̗̞͈̻̗Ḷ͙͎̯̹̞͓G̻O̭̗̮',
+  },
+  {
+    id: 13,
+    firstName: 'Mercedes',
+    lastName: 'Rossbrooke',
+    lastMessage: '🐵 🙈 🙉 🙊',
+  },
+  {
+    id: 14,
+    firstName: 'Hayward',
+    lastName: 'Hembry',
+    lastMessage: '̦H̬̤̗̤͝e͜ ̜̥̝̻͍̟́w̕h̖̯͓o̝͙̖͎̱̮ ҉̺̙̞̟͈W̷̼̭a̺̪͍į͈͕̭͙̯̜t̶̼̮s̘͙͖̕ ̠̫̠B̻͍͙͉̳ͅe̵h̵̬͇̫͙i̹͓̳̳̮͎̫̕n͟d̴̪̜̖ ̰͉̩͇͙̲͞ͅT͖̼͓̪͢h͏͓̮̻e̬̝̟ͅ ̤̹̝W͙̞̝͔͇͝ͅa͏͓͔̹̼̣l̴͔̰̤̟͔ḽ̫.͕',
+  },
+  {
+    id: 15,
+    firstName: 'Warren',
+    lastName: 'Cormode',
+    lastMessage: 'בְּרֵאשִׁית, בָּרָא אֱלֹהִים, אֵת הַשָּׁמַיִם, וְאֵת הָאָרֶץ',
+  },
+  {
+    id: 16,
+    firstName: 'Nomi',
+    lastName: 'Angell',
+    lastMessage: '사회과학원 어학연구소',
+  },
+  {
+    id: 17,
+    firstName: 'Peterus',
+    lastName: 'Pedrol',
+    lastMessage: 'Z̮̞̠͙͔ͅḀ̗̞͈̻̗Ḷ͙͎̯̹̞͓G̻O̭̗̮',
+  },
+  {
+    id: 18,
+    firstName: 'Christi',
+    lastName: 'Cullen',
+    lastMessage: ',。・:*:・゜’( ☻ ω ☻ )。・:*:・゜’',
+  },
+  {
+    id: 19,
+    firstName: 'Quincey',
+    lastName: 'Barthelme',
+    lastMessage: '𠜎𠜱𠝹𠱓𠱸𠲖𠳏',
+  },
+  {
+    id: 20,
+    firstName: 'Annora',
+    lastName: 'Beldan',
+    lastMessage: '␡',
+  },
+];
+
+export const messages = [
+  {
+    id: 353,
+    message:
+      'Ullamco pariatur ullamco laboris incididunt cillum sit sint nulla irure dolor commodo cupidatat. Non qui occaecat velit esse pariatur ex. Voluptate sit sint pariatur fugiat nisi sit ut anim. Dolore ex qui voluptate dolor cillum ipsum labore do. Eiusmod proident incididunt duis quis minim enim dolor mollit id do magna exercitation adipisicing est. Est exercitation anim do aliquip minim aliqua voluptate eu excepteur consectetur culpa. Sit anim voluptate sit enim duis ipsum in adipisicing culpa sunt reprehenderit nisi.\r\n',
+  },
+  {
+    id: 17,
+    message:
+      'Consectetur irure duis aliqua enim commodo dolore ea cupidatat labore. Eu ut laborum ullamco qui esse. Minim eu Lorem aliquip voluptate minim tempor aliqua aliquip sunt. Ad Lorem irure excepteur ea aliquip non minim occaecat occaecat ea occaecat aliquip veniam. Elit consequat dolor voluptate ullamco elit quis occaecat sunt veniam occaecat officia aute deserunt nisi. Reprehenderit proident est quis sit.\r\n',
+  },
+  {
+    id: 594,
+    message:
+      'Pariatur sint esse officia eu. Ut sint aute non duis nulla. Dolor velit aliquip dolor tempor enim culpa culpa. Duis ipsum est proident proident aliqua est ad. Aute elit voluptate tempor veniam sint cupidatat ad adipisicing. Aliqua commodo culpa incididunt tempor consequat anim voluptate veniam nostrud do velit. Occaecat ea eu nostrud Lorem officia sint voluptate exercitation deserunt sunt ipsum eiusmod aliqua.\r\n',
+  },
+  {
+    id: 485,
+    message:
+      'Anim labore Lorem dolor id laborum. Sunt do eiusmod mollit amet culpa aute officia. Qui irure nostrud occaecat eiusmod excepteur incididunt enim. Ullamco proident laboris veniam est enim tempor Lorem ea. Ipsum reprehenderit adipisicing non commodo qui tempor in eu exercitation. Adipisicing aute duis amet excepteur dolor cupidatat pariatur ut consequat ullamco exercitation do labore reprehenderit. Mollit laboris minim fugiat ad laboris et cupidatat adipisicing laboris tempor incididunt proident.\r\n',
+  },
+  {
+    id: 937,
+    message:
+      'Dolor quis nisi occaecat esse irure. Anim irure anim laboris laboris sunt mollit magna consequat do quis excepteur cillum. Non veniam proident sit nulla deserunt deserunt eu ipsum ex dolore duis sunt sit ullamco. Et tempor proident aute proident ut.\r\n',
+  },
+  {
+    id: 869,
+    message:
+      'Exercitation excepteur do dolore quis cillum consectetur ad occaecat fugiat non labore nostrud adipisicing. Id nostrud veniam veniam amet elit ut tempor deserunt qui. Do est laboris reprehenderit minim tempor enim deserunt nostrud nisi. Amet nostrud aute consectetur adipisicing velit est.\r\n',
+  },
+  {
+    id: 641,
+    message:
+      'Lorem sunt sunt adipisicing amet officia. Elit occaecat ex id voluptate qui aliquip adipisicing. Adipisicing aliquip commodo in nostrud do labore nulla non officia ad. In mollit eiusmod exercitation in est nisi. Adipisicing esse eiusmod et tempor veniam id amet minim est magna. Ullamco est veniam sint elit in Lorem in nisi pariatur est enim incididunt.\r\n',
+  },
+  {
+    id: 632,
+    message:
+      'Eu laborum ipsum anim cillum adipisicing id cupidatat deserunt sint sint nisi. Elit cupidatat proident dolore ea ea deserunt culpa est do labore. Occaecat aute duis sint do est ex cillum dolor dolor. Nostrud officia irure consectetur cupidatat nisi ea ad. Qui tempor adipisicing tempor occaecat dolor anim.\r\n',
+  },
+  {
+    id: 606,
+    message:
+      'Dolore anim qui veniam qui consectetur aute qui nulla proident dolore velit culpa nisi eu. Commodo quis minim velit ad nulla sint. Exercitation nisi eu cupidatat pariatur reprehenderit irure consequat sunt culpa ullamco eiusmod dolor officia. Lorem proident excepteur duis est aliquip in amet officia quis. Fugiat excepteur aute reprehenderit incididunt mollit. Excepteur consequat quis occaecat eiusmod aute ad tempor.\r\n',
+  },
+  {
+    id: 409,
+    message:
+      'Do excepteur nostrud pariatur velit est consequat in velit labore eiusmod consequat cillum. Ad officia eu tempor consectetur irure do aliquip aute id. Consequat laboris velit ea voluptate enim officia ex quis nostrud. Est laborum voluptate id irure Lorem deserunt nisi labore ullamco proident veniam eiusmod. Sint voluptate duis irure officia.\r\n',
+  },
+  {
+    id: 718,
+    message:
+      'Proident aliqua labore qui labore ad proident est anim. Commodo ut irure enim do qui deserunt ipsum Lorem. Labore et excepteur esse sint culpa officia exercitation magna anim tempor sunt. Ex occaecat aliqua amet ea ea sit id adipisicing incididunt.\r\n',
+  },
+  {
+    id: 272,
+    message:
+      'Sunt aliqua minim incididunt irure qui consectetur. Veniam reprehenderit sit aliquip officia cupidatat et laboris minim velit. Occaecat exercitation do adipisicing eu elit labore nostrud occaecat voluptate id proident magna dolore. Nostrud deserunt esse sint velit labore deserunt commodo sunt nostrud anim ad est.\r\n',
+  },
+  {
+    id: 920,
+    message:
+      'Ullamco adipisicing duis nulla et ut esse. Laboris consectetur ea veniam exercitation cupidatat ex amet sunt. Labore amet aliquip amet id consectetur cillum sunt. Qui nulla dolore anim nulla in.\r\n',
+  },
+  {
+    id: 376,
+    message:
+      'Excepteur elit sit dolore ut consectetur aute. Nostrud occaecat commodo duis fugiat adipisicing velit quis enim dolor ex laborum sunt irure occaecat. Minim ut quis voluptate aliqua sit do ea est fugiat esse nostrud sunt.\r\n',
+  },
+  {
+    id: 728,
+    message:
+      'Elit duis nostrud aliqua magna eu voluptate ipsum cillum deserunt mollit. Do adipisicing amet ut laboris reprehenderit exercitation adipisicing velit. Aliqua sint occaecat sunt labore proident culpa duis excepteur in incididunt veniam dolore exercitation. Ex incididunt consectetur commodo duis aliqua id. Enim fugiat aliquip nisi ut cillum ullamco voluptate laborum Lorem duis nostrud incididunt nostrud commodo. Aliquip ullamco do ex officia reprehenderit laborum eu excepteur velit voluptate magna commodo Lorem ex. Ex sint proident anim labore occaecat aliquip occaecat culpa ipsum dolore fugiat proident.\r\n',
+  },
+  {
+    id: 463,
+    message:
+      'Eu est incididunt veniam cupidatat eiusmod sit officia ad nisi veniam dolor irure irure anim. Duis cupidatat consequat cillum et dolor fugiat consectetur deserunt. Consequat aute adipisicing in ex fugiat mollit commodo fugiat. Consectetur amet fugiat adipisicing eu fugiat laborum anim. Fugiat in nostrud occaecat ex aliqua. Dolor sint irure et exercitation nostrud consequat qui cupidatat culpa culpa do ea enim. Ex ut incididunt minim qui pariatur consectetur ad Lorem aliquip in dolor eiusmod ipsum ex.\r\n',
+  },
+  {
+    id: 322,
+    message:
+      'Consequat commodo do pariatur irure occaecat Lorem officia velit consectetur irure cupidatat. Nostrud id anim consectetur anim ad ut eu dolore amet ipsum dolor est proident ut. Consequat voluptate reprehenderit deserunt id dolor magna incididunt voluptate qui quis voluptate consectetur. Eiusmod commodo eu fugiat anim dolore ut eu nostrud deserunt eiusmod do irure.\r\n',
+  },
+  {
+    id: 95,
+    message:
+      'Incididunt elit ipsum ad culpa esse qui amet sint ullamco cillum voluptate est aliquip. Lorem pariatur minim qui cupidatat in commodo ut magna laboris nulla cillum labore. Consectetur cupidatat sint deserunt nostrud consectetur eiusmod Lorem voluptate mollit sint consectetur. Sint reprehenderit aliqua enim ex deserunt esse sint deserunt consequat laborum. Exercitation aute deserunt laborum in cillum.\r\n',
+  },
+  {
+    id: 223,
+    message:
+      'Reprehenderit exercitation duis irure id incididunt consectetur commodo mollit Lorem eu proident incididunt. Eu est magna anim nisi Lorem. Elit et enim Lorem do sit excepteur sit. Sit ad nulla et adipisicing et labore.\r\n',
+  },
+  {
+    id: 21,
+    message:
+      'Non ut non cupidatat fugiat tempor irure magna velit. Culpa dolore aliqua et aliquip excepteur duis id non labore. Adipisicing fugiat labore est minim dolor voluptate amet incididunt veniam sint esse non incididunt. Excepteur consectetur Lorem nulla sit deserunt in eu ullamco minim ullamco fugiat id consectetur occaecat. Consectetur consequat consequat id veniam irure eiusmod veniam.\r\n',
+  },
+];
