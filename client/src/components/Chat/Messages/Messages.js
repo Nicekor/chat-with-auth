911656585b9ec08a06fb00a7134b72039821d2c4
Nicekor <zemorais16@hotmail.com>
2020-06-27 16:00:11 +0100

    Messages List component as well as each Message component.
    Undone and subject to changes

---
+++ client/src/components/Chat/Messages/Messages.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { List, makeStyles, Paper } from '@material-ui/core';
+import Message from './Message/Message';
+import Scrollbar from '../../UI/Scrollbar/Scrollbar';
+import { messages } from '../../../utils/dummyData';
+
+const useStyles = makeStyles((theme) => {
+  return {
+    messagesWrapper: {
+      width: '100%',
+      height: `calc(100vh - ${theme.measures.headerMaxHeight})`,
+      overflow: 'auto',
+      backgroundColor: theme.palette.background.default,
+    },
+  };
+});
+
+const Messages = () => {
+  const classes = useStyles();
+
+  return (
+    <Paper elevation={2} className={classes.messagesWrapper}>
+    {/* scrollbar should work from bottom to top */}
+      <Scrollbar>
+        <List>
+          {messages.map(({ id, message }) => {
+            return <Message key={id} message={{ id, message }} />;
+          })}
+        </List>
+      </Scrollbar>
+    </Paper>
+  );
+};
+
+export default Messages;
