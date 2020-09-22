83862c4b001af4660abfe8fea1f6e746ad9e4777
Jose <zemorais16@hotmail.com>
2020-09-22 15:39:46 +0100

    Refactored Not Found to have a mobile first layout

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -1,32 +1,71 @@
 import React from 'react';
+import {
+  Typography,
+  makeStyles,
+  useMediaQuery,
+  Box,
+  Button,
+  useTheme,
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
-import helpIcon from '../../assets/img/help-icon.png';
-import { Typography, makeStyles, useMediaQuery } from '@material-ui/core';
+import img404 from '../../assets/img/404.svg';
 
 const useStyles = makeStyles((theme) => {
   return {
     wrapper: {
       display: 'flex',
+      flexDirection: 'column',
       alignItems: 'center',
       margin: 'auto',
+      paddingBottom: theme.measures.header.height,
+    },
+    innerWrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    img: {
+      maxWidth: '10em',
+    },
+    btn: {
+      margin: theme.spacing(1),
     },
   };
 });
 
 const NotFound = () => {
-  const minWidth768 = useMediaQuery('(min-width:768px)');
   const classes = useStyles();
+  const minWidth768px = useMediaQuery('(min-width: 768px)');
+  const theme = useTheme();
 
   return (
-    <div
-      className={classes.wrapper}
-      style={{ flexDirection: minWidth768 ? '' : 'column' }}
-    >
-      <img src={helpIcon} alt="Question Mark Cat" />
-      <Typography variant="h2" align="center">
-        Page Not Found
-      </Typography>
-    </div>
+    <Box className={classes.wrapper}>
+      <Box
+        className={classes.innerWrapper}
+        style={minWidth768px ? { flexDirection: 'row' } : null}
+      >
+        <img
+          src={img404}
+          alt="404 Page"
+          className={classes.img}
+          style={minWidth768px ? { marginRight: theme.spacing(3) } : null}
+        />
+        <Typography variant="h2" align="center">
+          Page Not Found
+        </Typography>
+      </Box>
+      <Button
+        variant="contained"
+        color="secondary"
+        component={Link}
+        to="/"
+        className={classes.btn}
+        style={minWidth768px ? { alignSelf: 'flex-end' } : null}
+      >
+        Go Back Home
+      </Button>
+    </Box>
   );
 };
 
