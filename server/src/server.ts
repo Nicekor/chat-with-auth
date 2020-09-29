934a754659ccd53790f73462d3d6855045712b71
Jose <zemorais16@hotmail.com>
2020-09-29 11:13:40 +0100

    Added middlwares and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
-import express, { Application, Request, Response, NextFunction } from 'express'
+import express, { Application, Response } from 'express';
 import http, { Server as httpServer } from 'http';
 import io, { Server as SocketIOServer } from 'socket.io';
-import { config } from 'dotenv'
+import { config } from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
+import authRoute from './routes/auth';
 
 const app: Application = express();
 const server: httpServer = http.createServer(app);
@@ -9,13 +12,18 @@ const dotEnvConfig: object = config();
 const socketIO: SocketIOServer = io(server);
 
 // middlewares
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+
+// routes
+app.use('/api/auth', authRoute);
 
 socketIO.on('connection', (socket): void => {
-  console.log('A user connected')
-})
+  console.log('A user connected');
+});
 
-const port: number = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+const port: number = parseInt(<string>process.env.PORT) || 5000;
 server.listen(port, (): void => {
   console.log(`Server listening at http://localhost:${port}`);
-})
+});
