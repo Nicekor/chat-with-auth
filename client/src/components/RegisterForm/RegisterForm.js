dbdf93b4456f15051f3b7a04a2447c8cdad2fd51
Jose <zemorais16@hotmail.com>
2020-09-29 11:23:22 +0100

    RegisterForm component

---
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,159 @@
+import React, { useState, useContext, useCallback } from 'react';
+import { Button, makeStyles, Box } from '@material-ui/core';
+import useForm from '../../hooks/useForm';
+import { AuthContext } from '../../context/Auth';
+import { useHistory } from 'react-router-dom';
+
+import MyInput from '../UI/MyInput/MyInput';
+import PasswordInput from '../UI/PasswordInput/PasswordInput';
+
+const useStyles = makeStyles((theme) => {
+  return {
+    wrapper: {
+      margin: 'auto',
+      paddingBottom: theme.measures.header.height,
+    },
+    registerForm: {
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor: theme.palette.background.paper,
+      borderRadius: theme.spacing(2),
+      padding: theme.spacing(2),
+    },
+  };
+});
+
+const RegisterForm = () => {
+  const classes = useStyles();
+  const { authenticate } = useContext(AuthContext);
+  const history = useHistory();
+  const [errors, setErrors] = useState({});
+
+  const { values, handleFormChange, handleFormSubmit } = useForm(async () => {
+    try {
+      const { userId, errors } = await registerUser(values);
+      if (errors) {
+        setErrors(errors);
+      }
+      if (userId) {
+        authenticate(userId, () => {
+          history.push('/chats');
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  const registerUser = useCallback(async (userData) => {
+    try {
+      const res = await fetch('http://localhost:5000/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+      return res.json();
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  return (
+    <Box className={classes.wrapper}>
+      <form
+        onSubmit={handleFormSubmit}
+        onChange={handleFormChange}
+        noValidate
+        autoComplete="off"
+        className={classes.registerForm}
+        spellCheck={false}
+      >
+        <MyInput
+          color="primary"
+          required
+          error={!!errors.name}
+          inputLabelProps={{
+            htmlFor: 'name',
+            children: 'Name',
+          }}
+          inputProps={{
+            id: 'name',
+            type: 'text',
+            name: 'name',
+            autoFocus: true,
+            'aria-describedby': 'name-helper-text',
+          }}
+          formHelperTextProps={{
+            id: 'name-helper-text',
+            children: errors.name || "We'll never share your name",
+          }}
+        />
+        <MyInput
+          color="primary"
+          required
+          error={!!errors.email}
+          inputLabelProps={{
+            htmlFor: 'name',
+            children: 'Email address',
+          }}
+          inputProps={{
+            id: 'email',
+            type: 'email',
+            name: 'email',
+            'aria-describedby': 'email-helper-text',
+          }}
+          formHelperTextProps={{
+            id: 'email-helper-text',
+            children: errors.email || "We'll never share your email",
+          }}
+        />
+
+        <PasswordInput
+          color="primary"
+          required
+          error={!!errors.password}
+          inputLabelProps={{
+            htmlFor: 'password',
+            children: 'Password',
+          }}
+          inputProps={{
+            id: 'password',
+            name: 'password',
+            'aria-describedby': 'password-helper-text',
+          }}
+          formHelperTextProps={{
+            id: 'password-helper-text',
+            children: errors.password || "We'll never share your password.",
+          }}
+        />
+
+        <PasswordInput
+          color="primary"
+          required
+          error={!!errors.repeatPassword}
+          inputLabelProps={{
+            htmlFor: 'repeat-password',
+            children: 'Repeat Password',
+          }}
+          inputProps={{
+            id: 'repeat-password',
+            name: 'repeatPassword',
+            'aria-describedby': 'repeat-password-helper-text',
+          }}
+          formHelperTextProps={{
+            id: 'repeat-password-helper-text',
+            children:
+              errors.repeatPassword || "We'll never share your password",
+          }}
+        />
+        <Button type="submit" color="primary">
+          Register
+        </Button>
+      </form>
+    </Box>
+  );
+};
+
+export default RegisterForm;
