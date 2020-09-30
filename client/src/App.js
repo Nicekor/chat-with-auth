fc712dfac9635c099ca30fc4c2b30fc4f6c0f773
Jose <zemorais16@hotmail.com>
2020-09-30 20:09:31 +0100

    Removed AuthProvider and added Register path

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,10 @@ import { getThemes } from './utils/themes';
 
 import Header from './components/Header/Header';
 import LoginForm from './components/LogInForm/LogInForm';
+import RegisterForm from './components/RegisterForm/RegisterForm';
 import Chats from './components/Chats/Chats';
 import NotFound from './components/NotFound/NotFound';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import AuthProvider from './context/Auth';
 import EnterNickname from './components/EnterNickname/EnterNickname';
 import Chat from './components/Chats/Chat/Chat';
 
@@ -21,27 +21,26 @@ function App() {
     <MuiThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <CssBaseline />
       <BrowserRouter>
-        <AuthProvider>
-          <Switch>
-            <Route path="/" exact>
-              <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-              <LoginForm />
-            </Route>
-            <PrivateRoute path="/nickname">
-              <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-              <EnterNickname />
-            </PrivateRoute>
-            <Route path="/chats" exact>
-              <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-              <Chats />
-            </Route>
-            <Route path="/chat/:chatId" component={Chat} />
-            {/* <PrivateRoute path="/chats">
-              <Chats />
-            </PrivateRoute> */}
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </AuthProvider>
+        <Switch>
+          <Route path="/" exact>
+            <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+            <LoginForm />
+          </Route>
+          <Route path="/register" exact>
+            <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+            <RegisterForm />
+          </Route>
+          <Route path="/nickname" exact>
+            <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+            <EnterNickname />
+          </Route>
+          <PrivateRoute path="/chats" exact>
+            <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+            <Chats />
+          </PrivateRoute>
+          <PrivateRoute path="/chat/:chatId" component={Chat} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
   );
