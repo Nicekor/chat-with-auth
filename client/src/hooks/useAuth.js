801cdabdb93810221b7239825f2ae7e427f22bc1
Jose <zemorais16@hotmail.com>
2020-09-30 20:12:32 +0100

    Removed AuthContext, using a useAuth hook instead

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,69 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const useAuth = (authDoneCb) => {
+  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+  const history = useHistory();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      history.replace('/chats');
+    }
+  }, [isAuthenticated, history]);
+
+  const loginUser = async (userData) => {
+    try {
+      const res = await fetch('http://192.168.1.157:5000/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+      return res.json();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const registerUser = async (userData) => {
+    try {
+      const res = await fetch('http://192.168.1.157:5000/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+      return res.json();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const authenticate = async () => {
+    try {
+      const res = await fetch(
+        'http://192.168.1.157:5000/api/auth/authenticate',
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }
+      );
+
+      if (res.ok) {
+        // const authData = await res.json();
+        localStorage.setItem('isAuthenticated', true);
+        authDoneCb();
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return { isAuthenticated, authenticate, loginUser, registerUser };
+};
+
+export default useAuth;
