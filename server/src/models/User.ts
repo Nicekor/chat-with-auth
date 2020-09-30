0d7b17cf20b514590159ac0735a66ed9514492eb
Jose <zemorais16@hotmail.com>
2020-09-30 20:18:07 +0100

    Generating id and created_at values on the database instead and throwing error instead of logging it.

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,35 +1,32 @@
 import { QueryResult } from 'pg';
-import { v4 as uuidv4 } from 'uuid';
 import pool from '../db/index';
 
 class User {
-  id: string;
+  user_id?: string;
   name: string;
   email: string;
   password: string;
-  created_at: Date;
+  created_at?: Date;
 
   constructor(name: string, email: string, password: string) {
-    this.id = uuidv4();
     this.name = name;
     this.email = email;
     this.password = password;
-    this.created_at = new Date();
   }
 
-  async save(): Promise<User | void> {
+  async save(): Promise<User> {
     try {
       const res: QueryResult = await pool.query(
-        'INSERT INTO user_login (id, name, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
-        [this.id, this.name, this.email, this.password]
+        'INSERT INTO user_login (name, email, password) VALUES ($1, $2, $3) RETURNING *',
+        [this.name, this.email, this.password]
       );
       return res.rows[0];
     } catch (err) {
-      console.error(err.stack);
+      throw new Error(err);
     }
   }
 
-  static async findOne(email: string): Promise<User | undefined> {
+  static async findOne(email: string): Promise<User> {
     try {
       const res: QueryResult = await pool.query(
         'SELECT * FROM user_login WHERE email = $1',
@@ -37,7 +34,7 @@ class User {
       );
       return res.rows[0];
     } catch (err) {
-      console.error(err.stack);
+      throw new Error(err);
     }
   }
 }
