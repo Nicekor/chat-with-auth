897df97d4def790fb21710ac26732df81e39b0d6
Jose <zemorais16@hotmail.com>
2020-10-02 16:33:01 +0100

    Some tweaks to the database structures as well as replacing name by first_name and last_name

--- server/src/db/sql_init.sql
+++ server/src/db/sql_init.sql
@@ -2,28 +2,19 @@ CREATE DATABASE chat_with_auth;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE user_login(
-  user_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+CREATE TABLE attachment (
+  attachment_id int PRIMARY KEY,
+  attachment_type varchar(255),
+  attachment_name varchar(255)
+);
+
+CREATE TABLE user_login (
+  user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
   password VARCHAR(1024) NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW()
+  created_at timestamp DEFAULT NOW(),
+  avatar_id int REFERENCES attachment (attachment_id)
 );
 
-CREATE TABLE attachment(
-  attachment_id INT PRIMARY KEY,
-  attachment_type VARCHAR(255),
-  attachment_name VARCHAR(255)
-);
-
-CREATE TABLE addressee(
-  addressee_id uuid,
-  last_message VARCHAR(255),
-  avatar_id INT,
-  CONSTRAINT fk_user
-    FOREIGN KEY(addressee_id)
-    REFERENCES user_login(user_id),
-  CONSTRAINT fk_attachment
-    FOREIGN KEY (avatar_id)
-    REFERENCES attachment(attachment_id)
-);
