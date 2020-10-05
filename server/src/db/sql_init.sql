13e90b7e8f32478e5388be8310f592652142343c
Jose <zemorais16@hotmail.com>
2020-10-05 17:52:54 +0100

    Restructured database

--- server/src/db/sql_init.sql
+++ server/src/db/sql_init.sql
@@ -2,19 +2,20 @@ CREATE DATABASE chat_with_auth;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE attachment (
-  attachment_id int PRIMARY KEY,
-  attachment_type varchar(255),
-  attachment_name varchar(255)
-);
-
 CREATE TABLE user_login (
   user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
   password VARCHAR(1024) NOT NULL,
-  created_at timestamp DEFAULT NOW(),
-  avatar_id int REFERENCES attachment (attachment_id)
+  created_at timestamp DEFAULT NOW()
 );
 
+CREATE TABLE attachment (
+  attachment_id serial PRIMARY KEY,
+  attachment_type varchar(255),
+  attachment_mimetype varchar(255),
+  attachment_name varchar(255),
+  attachment_description varchar(255),
+  created_by uuid REFERENCES user_login (user_id)
+);
