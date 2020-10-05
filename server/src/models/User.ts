8dfb993a347db754cbedd94988d4a589c496b6f0
Jose <zemorais16@hotmail.com>
2020-10-05 17:58:02 +0100

    Refactored User model to use knex and added method to get the user name from the database

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,5 +1,5 @@
-import { QueryResult } from 'pg';
-import pool from '../db/index';
+import knexPg from '../db/index';
+import { UserName, UserNameOptions } from '../interfaces/user.interface';
 
 class User {
   user_id?: string;
@@ -8,42 +8,66 @@ class User {
   email: string;
   password: string;
   created_at?: Date;
-  avatar_id?: number;
 
   constructor(
-    first_name: string,
-    last_name: string,
+    firstName: string,
+    lastName: string,
     email: string,
     password: string
   ) {
-    this.first_name = first_name;
-    this.last_name = last_name;
+    this.first_name = firstName;
+    this.last_name = lastName;
     this.email = email;
     this.password = password;
   }
 
   async save(): Promise<User> {
     try {
-      const res: QueryResult = await pool.query(
-        'INSERT INTO user_login (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
-        [this.first_name, this.last_name, this.email, this.password]
-      );
-      return res.rows[0];
+      const savedUserRows: User[] = await knexPg
+        .insert({
+          first_name: this.first_name,
+          last_name: this.last_name,
+          email: this.email,
+          password: this.password,
+        })
+        .into<User>('user_login')
+        .returning('*');
+      return savedUserRows[0];
     } catch (err) {
       throw new Error(err);
     }
   }
 
-  static async findOne(email: string): Promise<User> {
+  static async findOne(newEmail: string): Promise<User | undefined> {
     try {
-      const res: QueryResult = await pool.query(
-        'SELECT * FROM user_login WHERE email = $1',
-        [email]
-      );
-      return res.rows[0];
+      const user: User | undefined = await knexPg
+        .from<User>('user_login')
+        .where({ email: newEmail })
+        .first();
+      return user;
     } catch (err) {
       throw new Error(err);
     }
+  }
+
+  static async getUserName(
+    userId: string,
+    options: UserNameOptions
+  ): Promise<string> {
+    const columns: (string | false | undefined)[] = [
+      options.firstName && 'first_name',
+      options.lastName && 'last_name',
+      options.fullName && 'first_name',
+      options.fullName && 'last_name',
+    ].filter(Boolean);
+
+    const userName: UserName = (await knexPg
+      .select(columns)
+      .from<User>('user_login')
+      .where({ user_id: userId })
+      .first()) as UserName;
+
+    return Object.values(userName).join(' ');
   }
 }
 
