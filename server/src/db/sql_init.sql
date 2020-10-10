9c49e90e411f8ddd3dfb436f30238820003707e6
Jose <zemorais16@hotmail.com>
2020-10-10 13:57:57 +0100

    Added friend_request table

--- server/src/db/sql_init.sql
+++ server/src/db/sql_init.sql
@@ -4,9 +4,10 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE user_login (
   user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+  friend_tag varchar(10) UNIQUE,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
+  email varchar(255) NOT NULL UNIQUE,
   password VARCHAR(1024) NOT NULL,
   created_at timestamp DEFAULT NOW()
 );
@@ -19,3 +20,11 @@ CREATE TABLE attachment (
   attachment_description varchar(255),
   created_by uuid REFERENCES user_login (user_id)
 );
+
+CREATE TABLE friend_request (
+  friend_request_id serial PRIMARY KEY,
+  requester_friend_tag varchar(10) UNIQUE,
+  recipient_friend_tag varchar(10) REFERENCES user_login (friend_tag),
+  accepted boolean DEFAULT FALSE
+);
+
