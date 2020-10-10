3baa25da265bafe2571883ec378f5b7173dc09ca
Jose <zemorais16@hotmail.com>
2020-10-10 14:00:15 +0100

    Add Friend validation

--- server/src/validation/validation.ts
+++ server/src/validation/validation.ts
@@ -1,10 +1,10 @@
-import Joi, { object, ObjectSchema, options, ValidationResult } from 'joi';
+import Joi, { ObjectSchema, StringSchema, ValidationResult } from 'joi';
 import User from '../models/User';
 
 // todo: check issue with the repeat password error message
 
 export const registerValidation = (data: User): ValidationResult => {
-  const schema: ObjectSchema = object({
+  const schema: ObjectSchema = Joi.object({
     firstName: Joi.string().alphanum().required().label('First Name'),
     lastName: Joi.string().alphanum().required().label('Last Name'),
     email: Joi.string().required().email().label('Email address'),
@@ -27,13 +27,31 @@ export const registerValidation = (data: User): ValidationResult => {
 };
 
 export const loginValidation = (data: User): ValidationResult => {
-  const schema: ObjectSchema = object({
+  const schema: ObjectSchema = Joi.object({
     email: Joi.string().required().email().label('Email address'),
     password: Joi.string().required().label('Password'),
   });
 
   return schema.validate(data, {
     abortEarly: false,
+    errors: {
+      wrap: {
+        label: false,
+      },
+    },
+  });
+};
+
+export const addFriendValidation = (
+  requesterFriendTag: string,
+  recipientFriendTag: string | null
+): ValidationResult => {
+  const schema: StringSchema = Joi.string()
+    .required()
+    .length(10)
+    .invalid(requesterFriendTag ? requesterFriendTag : null)
+    .label('Friend Tag');
+  return schema.validate(recipientFriendTag, {
     errors: {
       wrap: {
         label: false,
