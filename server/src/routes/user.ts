50757e154701a7fe7fc4194bc8c7b75cf22ecd4a
Jose <zemorais16@hotmail.com>
2020-10-10 14:00:02 +0100

    Added user routes for handling friend requests

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,6 +1,14 @@
 import { Request, Response, Router } from 'express';
+import { ValidationResult } from 'joi';
+import {
+  FriendRequestData,
+  FriendRequestDB,
+  FriendRequestUI,
+} from '../interfaces/friendRequest.interface';
 import verifyToken from '../middlewares/verifyToken';
+import FriendRequest from '../models/FriendRequest';
 import User from '../models/User';
+import { addFriendValidation } from '../validation/validation';
 
 const router = Router();
 
@@ -25,5 +33,101 @@ router.get(
     }
   }
 );
+
+router.get('/friend-tag', verifyToken, async (req, res) => {
+  const userId: string = res.locals.authData.userId;
+  try {
+    const userFriendTag: string = <string>await User.getFriendTag(userId);
+    return res.status(200).json(userFriendTag);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
+});
+
+router.get('/friend-requests', verifyToken, async (req, res) => {
+  const userId: string = res.locals.authData.userId;
+  try {
+    const userFriendTag: string = <string>await User.getFriendTag(userId);
+    const friendRequests: FriendRequestData[] = await FriendRequest.getFriendRequests(
+      userFriendTag
+    );
+    const friendRequestsFormatted: FriendRequestUI[] = friendRequests.map(
+      ({ first_name, last_name, attachment_name, ...rest }) => {
+        return {
+          name: [first_name, last_name].join(' '),
+          avatar: attachment_name,
+          ...rest,
+        };
+      }
+    );
+    return res.status(200).json(friendRequestsFormatted);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
+});
+
+router.post('/friend-request', verifyToken, async (req, res) => {
+  const userId: string = res.locals.authData.userId;
+  const recipientFriendTag: string = req.body.recipient;
+  try {
+    const userFriendTag: string = <string>await User.getFriendTag(userId);
+    const { error }: ValidationResult = addFriendValidation(
+      userFriendTag,
+      recipientFriendTag
+    );
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+    const friendRequestExists:
+      | FriendRequestDB
+      | undefined = await FriendRequest.findOne(
+      userFriendTag,
+      recipientFriendTag
+    );
+    if (friendRequestExists) {
+      return res
+        .status(400)
+        .json({
+          error: 'A friend request already exists or you are aldready friends.',
+        });
+    }
+    await FriendRequest.send(userFriendTag, recipientFriendTag);
+    return res.sendStatus(201);
+  } catch (err) {
+    if (err.code === '23505') {
+      return res.status(400).json({ error: "This Friend Tag doesn't exist." });
+    }
+    console.error(err);
+    return res.sendStatus(500);
+  }
+});
+
+router.delete('/friend-request', verifyToken, async (req, res) => {
+  const userId: string = res.locals.authData.userId;
+  const requesterFriendTag: string = req.body.requester;
+  try {
+    const recipientFriendTag: string = <string>await User.getFriendTag(userId);
+    await FriendRequest.delete(requesterFriendTag, recipientFriendTag);
+    return res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
+});
+
+router.put('/friend-request', verifyToken, async (req, res) => {
+  const userId: string = res.locals.authData.userId;
+  const requesterFriendTag: string = req.body.requester;
+  try {
+    const recipientFriendTag: string = <string>await User.getFriendTag(userId);
+    await FriendRequest.accept(requesterFriendTag, recipientFriendTag);
+    return res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
+});
 
 export default router;
