d5030900adc6efd9be7f411c02f2a59729d272cb
Jose <zemorais16@hotmail.com>
2020-10-13 22:47:24 +0100

    Deleted unused AvatarWithLetter

--- client/src/components/UI/AvatarWithLetter/AvatarWithLetter.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Avatar } from '@material-ui/core';
-
-const AvatarWithLetter = ({ personName, ...props }) => {
-  return <Avatar {...props}>{personName ? personName.charAt(0) : null}</Avatar>;
-};
-
-export default AvatarWithLetter;
