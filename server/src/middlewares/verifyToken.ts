bbd11d6c1b1a08c66bfad154745771da69112486
Jose <zemorais16@hotmail.com>
2020-10-13 22:46:10 +0100

    parameters types

--- server/src/middlewares/verifyToken.ts
+++ server/src/middlewares/verifyToken.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
-const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void | Response => {
   const bearerHeader: string | undefined = req.header('Authorization');
   if (!bearerHeader) {
     return res.sendStatus(403);
@@ -9,6 +13,7 @@ const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   const bearerToken: string | undefined = bearerHeader?.split(' ')[1];
 
   try {
+    // eslint-disable-next-line @typescript-eslint/ban-types
     const authData: string | object = jwt.verify(
       <string>bearerToken,
       <string>process.env.TOKEN_SECRET
